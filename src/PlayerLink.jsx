9d1334c5adda60ae402851193235d28a6ae64071
Kongtiao <kongtiao92@gmail.com>
2026-07-12 11:06:26 +0800

    Fix the gb player link

--- src/PlayerLink.jsx
+++ src/PlayerLink.jsx
@@ -1,7 +1,7 @@
 import { Link } from "@chakra-ui/react";
 
 function PlayerLink({ children, id }) {
-  return <Link href={"https://goldberries.net/playe/" + id} target="_blank" color="blue.solid">{children}</Link>
+  return <Link href={"https://goldberries.net/player/" + id} target="_blank" color="blue.solid">{children}</Link>
 }
 
 export default PlayerLink;
